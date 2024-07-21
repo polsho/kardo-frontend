fb8ea2efda61d90deb004221170ac1a06e09567a
Polina Shmeleva <polsho1988@gmail.com>
2024-07-21 12:32:07 +0300

    files formatted

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,28 +1,20 @@
-import React from 'react';
-  import './app.css';
-  import {
-    Routes,
-    Route,
-    useLocation,
-    useNavigate,
-    useParams
-  } from 'react-router-dom';
+import React from 'react'
+import './app.css'
+import { Routes, Route, useLocation, useNavigate, useParams } from 'react-router-dom'
 //   import { useDispatch } from '../../services/store';
 
-  import { useEffect } from 'react';
-  
-  function App() {
-    const location = useLocation();
-    const background = location.state?.background;
+import { useEffect } from 'react'
 
-    return (
-      <div className='app'>
-        {/* <AppHeader /> */}
-        <Routes location={background || location}>
-          {/* <Route path='/' element={<Main />} /> */}
-        </Routes>
-      </div>
-    );
-  };
-  
-  export default App;
+function App() {
+  const location = useLocation()
+  const background = location.state?.background
+
+  return (
+    <div className="app">
+      {/* <AppHeader /> */}
+      <Routes location={background || location}>{/* <Route path='/' element={<Main />} /> */}</Routes>
+    </div>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import './index.css';
-import React from 'react';
-import ReactDOMClient from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import './index.css'
+import React from 'react'
+import ReactDOMClient from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 // import store from './services/store';
-import App from './app/app';
+import App from './app/app'
 
-const container = document.getElementById('root') as HTMLElement;
-const root = ReactDOMClient.createRoot(container!);
+const container = document.getElementById('root') as HTMLElement
+const root = ReactDOMClient.createRoot(container!)
 
 root.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     {/* </Provider> */}
   </React.StrictMode>
-);
+)
