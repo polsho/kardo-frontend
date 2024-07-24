3ffe3144a7e0f6b9adb62f1281ee54eb487eac08
Polina Shmeleva <polsho1988@gmail.com>
2024-07-24 14:53:20 +0300

    modified css file

--- src/ui/tab-bar-button/tab-bar-button.tsx
+++ src/ui/tab-bar-button/tab-bar-button.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import './tab-bar-button.css'
+import styles from './tab-bar-button.module.css'
 import { TTabBarButtonProps } from './types'
 
 export function TabBarButton({ icon, title, onClick }: TTabBarButtonProps) {
   return (
-    <button className="tab-bar-button" onClick={onClick}>
+    <button className={styles.tabBarButton} onClick={onClick}>
       <img src={icon} alt="иконка" className="tab-bar-button__icon" />
       <div className="text_type_main-small">{title}</div>
     </button>
