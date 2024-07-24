fe8e142752cbd90651bda88136dcfcdfe1e38515
Polina Shmeleva <polsho1988@gmail.com>
2024-07-24 13:16:36 +0300

    added: TabBarButton ui

---
+++ src/ui/tab-bar-button/tab-bar-button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './tab-bar-button.css'
+import { TTabBarButtonProps } from './types'
+
+export function TabBarButton({ icon, title, onClick }: TTabBarButtonProps) {
+  return (
+    <button className='' onClick={onClick}>
+      
+    </button>
+  )
+}
---
+++ src/ui/tab-bar-button/types.ts
@@ -0,0 +1,6 @@
+
+export type TTabBarButtonProps = {
+    icon: string;
+    title: string;
+    onClick: () => void;
+  };
