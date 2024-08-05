6113835a7d85096a669ebe534f608f491673e7a6
Alina Abrashkina <lina.lina.malinaa@gmail.com>
2024-08-05 23:50:37 +0400

    added new functionality

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './post.module.css'
 import iconProfile from '../../assets/images/photo-profile.png'
 import clsx from 'clsx'
 import like from '../../assets/icons/like.svg'
 import dislike from '../../assets/icons/dislike.svg'
+import comments from '../../assets/icons/comments.svg'
+import settings from '../../assets/icons/nav-bar.svg'
+import bin from '../../assets/icons/delete.svg'
 
 export type TPost = {
   url?: string,
@@ -15,15 +18,39 @@ export type TPost = {
 
 export const Post: React.FC<TPost> = ({url, text, name, date, category}: TPost) => {
 
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleButton = (): void => {
+    if (!isActive) {
+      setIsActive(true);
+      console.log(1);
+    } else {
+      setIsActive(false);
+      console.log(2);
+    }
+  }
+
+  const getActiveClass = ({isActive} : {isActive: boolean}) => isActive ? styles.active : styles.inactive;
+
   return (
     <>
-        <div className={styles.post}>
-          <div className={clsx(styles.heading, "text_type_main-default")}>
-            <div className={styles.info}>
-              <img className={styles.icon_profile} src={iconProfile} alt="фото пользователя" />
+      <button className={clsx(styles.button, getActiveClass,  'text_type_main-large')}>
+        <img src={bin} alt="иконка корзина" />
+        Удалить публикацию
+      </button>
+
+      <div className={styles.post}>
+        <div className={clsx(styles.heading, 'text_type_main-default')}>
+          <div className={styles.info}>
+            <img className={styles.icon_profile} src={iconProfile} alt="фото пользователя" />
               <span>{name}</span>
             </div>
-            <span>{date}</span>
+
+            <div className={styles.info} onClick={(evt) => toggleButton()
+            }>
+              <span className={styles.date}>{date}</span>
+              <img src={settings} alt="настройки" />
+            </div>
           </div>
 
           {url ? <img className={styles.media} src={url} alt="картинка поста" /> :
@@ -36,13 +63,18 @@ export const Post: React.FC<TPost> = ({url, text, name, date, category}: TPost) 
 
             <div className={styles.reactions}>
 
-              <div className={clsx(styles.like_container, 'text_type_main-large')}>
-                <img src={like} alt="like"/>
+              <div className={clsx(styles.like_container, styles.actions_container, 'text_type_main-large')}>
+                <img src={like} alt="like" />
                 <span>21</span>
               </div>
 
-              <div className={clsx(styles.dislike_container, 'text_type_main-large')}>
+              <div className={clsx(styles.dislike_container, styles.actions_container, 'text_type_main-large')}>
                 <img src={dislike} alt="dislike" />
+                <span>18</span>
+              </div>
+
+              <div className={clsx(styles.dislike_container, styles.actions_container, 'text_type_main-large')}>
+                <img src={comments} alt="comments" />
                 <span>18</span>
               </div>
             </div>
