4df30142a1d949090427c8ab7b741ae3bb2fe671
Alina Abrashkina <lina.lina.malinaa@gmail.com>
2024-08-06 20:32:04 +0400

    added new routes

---
+++ src/pages/contest/contest.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./contest.module.css";
+
+export const Contest: React.FC = () => {
+
+  return (
+    <>
+      <p className={styles.heading}>Contest</p>
+      <div>
+      </div>
+    </>
+  )
+}
